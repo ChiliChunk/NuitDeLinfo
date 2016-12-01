666ceec5db9fa48cc0e7b03fb05de3bbb2bfb13e
clementlopez <lopezclement31@gmail.com>
2016-12-01 23:59:47 +0100

    Add files via upload

--- menus.php
+++ menus.php
@@ -1,8 +1,11 @@
 <nav class="sidebar">
 <!-- Menus.php -->
-  <ul>
-    <li><a href="index.php">Accueil</a></li>
-    <li><a href="PWS/TP/TP1/index.php">TP1 PWS</a></li>
-    <li><a href="PWS/TP/TP2/index.php">TP2 PWS</a></li>
+
+ <ul>
+ <?php
+ require-once("../fonctions.php");
+    afficherProjets();
+	?>
   </ul>
+
 </nav>
