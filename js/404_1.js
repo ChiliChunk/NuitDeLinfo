c8e6f7cb0e92e29e612b4cfc7712d16e4ec303b4
Denis <guiraudet@live.fr>
2016-12-02 00:19:51 +0100

    img

--- js/404_1.js
+++ js/404_1.js
@@ -28,3 +28,24 @@ function reload() {
 	location.reload();
 
 }
+
+function toRadians (angle) {
+	return angle * (Math.PI / 180);
+}
+
+function onclick_page(event) {
+	var xCurs = event.clientX;
+	var yCurs = event.clientY;
+	var image = document.getElementById("eyeLeft");
+	var posx = $(image).position().left;
+	var posy = $(image).position().top;
+	var difX = Math.abs(xCurs - posx);
+	var difY = Math.abs(yCurs - posy);
+	var tanAngle = difX/difY;
+	//conversion rad
+	var angle = Math.atan(tanAngle)*180/Math.PI;
+	if (xCurs > posx){
+		angle = -angle;
+	}
+	image.style.transform = "rotate("+angle+"deg)";
+}
